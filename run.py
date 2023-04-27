fe3b9192a1c0ad0aea89b51b7ba6d2ec2b3b7154
XyzonXD <131737930+Ramzgod@users.noreply.github.com>
2023-04-27 16:50:04 +0700

    Update run.py

--- run.py
+++ run.py
@@ -337,7 +337,7 @@ def login():
 			login123()
 		except requests.exceptions.ConnectionError:
 			li = ' [+] Lu Kagak Ada Sinyal Anjirt Silahkan Beli Sinyal Di Sugeng Kalo Mau Carck'
-			lo = ' [+] Salam BintangTzy And XyzonXD'
+			li = ' [+] Salam BintangTzy And XyzonXD'
 			lo = mark(li, style='red')
 			sol().print(lo, style='cyan')
 			exit()
